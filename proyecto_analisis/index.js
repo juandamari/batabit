d1532f4a0382bd22825f3fb1a3b746b99d492384
juan david M <juan_da-mari@hotmail.com>
2022-06-13 20:23:24 -0500

    estilando el cuadro

--- proyecto_analisis/index.js
+++ proyecto_analisis/index.js
@@ -1,5 +1,4 @@
 function capacidadDeuda (ingreso, egreso,porcentaje) {
-    /* const porcentaje = 0.35 */
     const capacidad = (ingreso - egreso) * porcentaje;
     return capacidad;
 };
@@ -18,27 +17,4 @@ function clickboton() {
     
     let capacidad = capacidadDeuda (enteringreso,enteregreso,enterporcentaje)
     resultCoupon.innerText =`capacidad de endeudamiento es de: $${capacidad} pesos`
-    
-}
-
-
-/* let porcentaje
-let capacidad;
-
-switch(true) {
-    case value === "1":
-      porcentaje = 0.20;
-      capacidad = capacidadDeuda(enteringreso, enteregreso, entercapacidad);
-      resultCoupon.innerText =`Su capacidad de ahorro es de maximo: ${capacidad} pesos, segun su nivel de ahorro conservador`
-    break;
-    case value === "2":
-        porcentaje = 0.30;
-        capacidad = capacidadDeuda(enteringreso, enteregreso, entercapacidad);
-        resultCoupon.innerText =`Su capacidad de ahorro es de maximo: ${capacidad} pesos, segun su nivel de ahorro moderado`
-    break;
-    default:
-        capacidad = capacidadDeuda(enteringreso, enteregreso, entercapacidad);
-        resultCoupon.innerText = `Su capacidad de ahorro es de maximo de ahorro de se puede calcular`;
-        break;
 };
-} */
